d9d26cc5f135160c2f9933ce6fb0a833b9be9b0c
Kelvin Liu <ycliuhw@gmail.com>
2018-02-21 18:53:35 +1100

    Kelvin: wireframe the renderer

---
+++ src/renderer.py
@@ -0,0 +1,74 @@
+import logging
+import re
+
+import troposphere
+from urllib import unquote
+# from troposphere import Base64, Join, Ref, GetAtt
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+
+__all__ = ('to_userdata', )
+
+
+class NotMatchError(Exception):
+    ...
+
+
+class Type(object):
+
+    name = None
+    pattern = None
+
+    @property
+    def split_exp(self):
+        return '{}\({}\)'.format(self.name, self.pattern)
+
+    @property
+    def match_exp(self):
+        return '^{}\(({})\)$'.format(self.name, self.pattern)
+
+    def _handle(self, value):
+        return getattr(troposphere, self.name)(unquote(value))
+
+    def do(self, value):
+        try:
+            return self._handle(re.match(self.match_exp, value).group(1))
+        except AttributeError as e:
+            raise NotMatchError(e.args)
+
+
+class Ref(Type):
+
+    name = 'Ref'
+    pattern = '[a-zA-Z0-9\-:%]+'
+
+
+class GetAtt(Type):
+
+    name = 'GetAtt'
+    pattern = '[a-zA-Z0-9, \-:%]+'
+
+    def _handle(self, value):
+        return getattr(troposphere, self.name)(*[unquote(k.strip()) for k in value.split(',') if k.strip()])
+
+
+SUPPORTED_ELEMENTS = (
+    Ref(),
+    GetAtt(),
+)
+
+
+def to_userdata(userdata_str, base64_encode=True):
+    splitted = re.split('(%s)' % ('|'.jojn(map(lambda e: e.split_exp, SUPPORTED_ELEMENTS))), userdata_str)
+
+    def check(value):
+        for e in SUPPORTED_ELEMENTS:
+            try:
+                return e.do(value)
+            except NotMatchError:
+                ...
+        return value
+
+    userdata = troposphere.Join('', list(map(check, splitted)))
+    return Base64(userdata) if base64_encode else userdata
